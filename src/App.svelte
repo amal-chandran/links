1d1f4c829b58f7e3bea4966895fd67f94bf831b1
Amal Chandran <amalchandrandev@gmail.com>
2022-09-17 14:50:20 +0530

    fix(stack): corrected links exclusive to social

--- src/App.svelte
+++ src/App.svelte
@@ -5,33 +5,34 @@
     faYoutube,
     faGithub,
   } from "@fortawesome/free-brands-svg-icons";
+
   let linkList = [
     {
-      name: "Instagram",
+      name: "weekend_explored",
+      subText: "Instagram",
       link: "https://www.instagram.com/weekend_explored",
       icon: faInstagram,
-      subText: "@weekend_explored",
       iconClassNames: "text-orange-500",
     },
     {
-      name: "Youtube",
-      link: "https://www.youtube.com/channel/UCkHTylmLVCsbyKXJq-Hmalg",
-      icon: faYoutube,
-      subText: "#weekend_explored",
-      iconClassNames: "text-red-500",
+      name: "behind_scenes_explored",
+      subText: "Instagram",
+      link: "https://www.instagram.com/behind_scenes_explored",
+      icon: faInstagram,
+      iconClassNames: "text-orange-500",
     },
     {
-      name: "Github",
-      link: "https://github.com/amal-chandran",
-      icon: faGithub,
-      subText: "#weekend_explored",
-      iconClassNames: "text-black-500",
+      name: "weekend_explored",
+      subText: "Youtube",
+      link: "https://www.youtube.com/channel/UCkHTylmLVCsbyKXJq-Hmalg",
+      icon: faYoutube,
+      iconClassNames: "text-red-500",
     },
   ];
 </script>
 
 <div class="h-screen">
-  <div class="md:w-1/3 mx-auto mt-20">
+  <div class="md:w-1/3  mx-auto mt-20">
     <div class="p-2 flex items-center flex-col">
       <img
         src="https://avatars.githubusercontent.com/u/29562232?s=400&u=eca0006297cc43aa65b1521b6829783728f9ea5f&v=4"
@@ -40,27 +41,28 @@
       />
       <div class="font-bold mt-2 opacity-90">Amal Chandran</div>
       <div class="my-2 text-center">
-        <span>Full Stack Developer</span> |
-        <span>Open Source Contributor</span> |
-        <span>Learning Content Creation</span>
+        <span>Beginner FPV</span> |
+        <span>Content Creator</span>
       </div>
     </div>
-    <div class="flex gap-y-2 flex-col w-1/2 mx-auto">
+    <div class="flex gap-y-2 flex-col xl:w-1/2 px-2 pb-2 md:mx-auto">
       {#each linkList as link}
         <a
           href={link.link}
-          class="flex items-center justify-center gap-2 bg-black bg-opacity-40 hover:bg-slate-900 rounded py-2 px-3"
+          class=" bg-black bg-opacity-40 hover:bg-slate-900 rounded py-2 px-3"
         >
-          <div>
-            <Fa icon={link.icon} class={link.iconClassNames} />
-          </div>
-          <div class="font-normal text-white ">
-            {link.name}
+          <div class="flex items-center justify-center  gap-2">
+            <div>
+              <Fa icon={link.icon} class={link.iconClassNames} />
+            </div>
+            <div class="font-normal text-white ">
+              {link.name}
+            </div>
           </div>
 
-          <!-- <div class="ml-auto">
+          <div class="text-gray-300 flex justify-center text-xs">
             {link.subText}
-          </div> -->
+          </div>
         </a>
       {/each}
     </div>
